aed43d1f173fd9ad952702f491d6de33378ae89e
Weston McNamee <ghost.squadron@gmail.com>
2019-04-26 20:11:59 -0700

    Implements p4

---
+++ pkg/problems/004/main.go
@@ -0,0 +1,93 @@
+package main
+
+//go:generate go build -buildmode=plugin -o main.so main.go
+
+import (
+	"strconv"
+	"context"
+	"github.com/ghostsquad/goeuler/pkg"
+)
+
+// This solution decrements only 1 factor at a time, until a palindrome is found.
+// Then it decrements both from the starting position, and continues.
+// If a palindrome is found at anytime and
+// a product lower than the lowest product from the previos run is created
+// we exit, as
+// it's not possible to create a larger palindrome once we reached that threshold
+
+// TODO implement logging
+
+type solution struct {}
+
+func (s solution) Solve(ctx context.Context) {
+	pkg.SolveWith(ctx, "004", func() int {
+		answer := 0
+		factor1StartPoint := 999
+		factor2StartPoint := factor1StartPoint
+		biggestProductPalindromeFromPreviousRun := 0
+
+		thresholdReached := false
+
+		for factor1 := factor1StartPoint; factor1 > 0; factor1-- {
+			factor2StartPoint = factor1
+
+			var product int
+
+			for factor2 := factor2StartPoint; factor2 > 0; factor2-- {
+				//fmt.Printf("Checking: %d x %d\n", factor1, factor2)
+
+				product = factor1 * factor2
+                //products are only getting smaller for each iteration
+                //break early if the product is smaller than the biggest product from the previous run
+				if product < biggestProductPalindromeFromPreviousRun {
+					//fmt.Printf("Lowest product previous run has been surpassed: %d < %d\n", product, biggestProductPalindromeFromPreviousRun)
+
+					// the threshold is reached if we've started this loop, and we are producing products
+					// lower then lowest produced products from previous runs
+					if factor2 == factor2StartPoint {
+						//fmt.Printf("Reached threshold: %d x %d = %d\n", factor1, factor2, product)
+						thresholdReached = true
+					}
+
+					break
+				}
+
+				if isPalindrome(product) {
+					//fmt.Printf("Found palindrome: %d\n", product)
+
+					if product > answer {
+						biggestProductPalindromeFromPreviousRun = product
+						answer = product
+					}
+					
+					break
+				}
+			}
+
+			if thresholdReached {
+				break
+			}
+		}
+
+		return answer
+	})
+}
+
+func isPalindrome(x int) bool {
+	str := strconv.Itoa(x)
+	rev := reverse(str)
+
+	return str == rev
+}
+
+func reverse(s string) string {
+    n := len(s)
+    runes := make([]rune, n)
+    for _, rune := range s {
+        n--
+        runes[n] = rune
+    }
+    return string(runes[n:])
+}
+
+var Solution solution
